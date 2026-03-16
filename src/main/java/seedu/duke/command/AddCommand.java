21410cbccefe05effc837d08996a4aa8ba44c804
ben <jwben03@gmail.com>
2026-03-16 17:00:28 +0800

    Abstract Add command into its own class

---
+++ src/main/java/seedu/duke/command/AddCommand.java
@@ -0,0 +1,39 @@
+package seedu.duke.command;
+
+import seedu.duke.transaction.Expense;
+import seedu.duke.transaction.Income;
+import seedu.duke.transactionlist.TransactionList;
+import seedu.duke.ui.Ui;
+
+import java.time.LocalDate;
+
+public class AddCommand extends Command {
+    private final String category;
+    private final double amount;
+    private final String description;
+    private final LocalDate date;
+
+    public AddCommand(String category, double amount, String description, LocalDate date) {
+        this.category = category;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+
+    @Override
+    public void execute(TransactionList list, Ui ui) {
+        if (category.equalsIgnoreCase("income")) {
+            Income income = new Income(category, amount, description, date);
+            list.add(income);
+            ui.showMessage("Added: " + income);
+        } else if (Expense.VALID_CATEGORIES.contains(category.toLowerCase())) {
+            Expense expense = new Expense(category, amount, description, date);
+            list.add(expense);
+            ui.showMessage("Added: " + expense);
+        } else {
+            ui.showMessage("Invalid category '" + category + "'."
+                    + " Valid categories: " + Expense.VALID_CATEGORIES);
+        }
+    }
+
+}
