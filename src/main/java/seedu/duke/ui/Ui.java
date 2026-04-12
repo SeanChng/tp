ea34ff36dec1b7b80cd3ec5c82c61e0faccdde8e
ben <jwben03@gmail.com>
2026-04-12 14:08:21 +0800

    Update the help function to display the help command too

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -92,7 +92,9 @@ public class Ui {
                                  - Generates all due recurring transactions up to today.
                 17. Filter     : `filter from/YYYY-MM-DD to/YYYY-MM-DD`
                                  - Filters your transactions based on the time frame given
-                18. Exit       : `exit`
+                18. Help       : `help`
+                                 - Generates the help message into the terminal
+                19. Exit       : `exit`
                                  - Exits the program.
                 %s""".formatted(separator, separator, separator);
 
